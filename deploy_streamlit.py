50ee5c15dcd190009318d111417788d4c5d17b7a
Andreu Vall Hernàndez <andreu.vallhernandez@gmail.com>
2023-11-11 21:24:48 +0100

    Enhancement of the deploy on Streamlit

--- deploy_streamlit.py
+++ deploy_streamlit.py
@@ -1,12 +1,19 @@
 import streamlit as st
-import os
 
-def listar_imagenes(dataset_path):
-    return [file for file in os.listdir(dataset_path) if file.endswith('.jpg') or file.endswith('.png')]
+import pandas as pd
+
+outfit_data = pd.read_csv('datathon/dataset/outfit_data.csv')
+product_data = pd.read_csv('datathon/dataset/product_data.csv')
+files_by_cat = product_data[['des_product_category', 'des_filename']].groupby('des_product_category').agg(lambda x: list(x))['des_filename']
+
 
 dataset_path = 'datathon/images/'
 
 st.title("Select the clothes for your outfit")
+
+option = st.selectbox("Select a category", list(files_by_cat.keys()))
+def listar_imagenes(dataset_path):
+    return files_by_cat[option][:20]
 
 if 'show_selector' not in st.session_state:
     st.session_state['show_selector'] = False
@@ -22,15 +29,16 @@ if st.session_state['show_selector']:
     cols = st.columns(3) 
     for index, image in enumerate(imagenes):
         col = cols[index % 3] 
-        img_path = os.path.join(dataset_path, image)
+        img_path = image
         if col.button(f"Select {index}", key=image):
+            # lst.session_state['show_selector'] = False
             if image not in st.session_state['selected_images']:
                 st.session_state['selected_images'].append(image)
         col.image(img_path, width=100) 
 
 st.subheader("Your Current Outfit")
 for image in st.session_state['selected_images']:
-    st.image(os.path.join(dataset_path, image), width=100)
+    st.image(image, width=100)
 
 num_recommendations = st.number_input("How many items do you want to be recommended?", min_value=1, max_value=10)
 
